39eb251ac511a41bf8e337652e3b5d0043b3085a
verenasworld <verena.felizidas@gmail.com>
2021-11-11 16:01:11 +0100

    convert to a switch statement

--- src/com/calcengine/Main.java
+++ src/com/calcengine/Main.java
@@ -5,23 +5,43 @@ public class Main {
     //build a system that's able to perform a variety of mathematical calculations.
     public static void main(String[] args) {
 
-        double value1 = 100.0d ;
-        double value2 = 50.0d;
-        double result;
+        double value1 = 100.0d;
+        double value2 = 0.0d;
+        double result = 0.0d;
         char opCode = 'd';
 
 
-        if(opCode == 'a')
-            result = value1 + value2;
-        else if( opCode == 's')
-            result = value1 - value2;
-        else if( opCode == 'm')
-            result = value1 * value2;
-        else if( opCode == 'd')
-            result = value1 / value2;
-        else
-            result = 0.0d;
+        switch (opCode){
+            case 'a' :
+                result = value1 + value2;
+                break;
+            case  's' :
+                result = value1 - value2;
+                break;
+            case 'm':
+                result = value1 * value2;
+                break;
+            case 'd':
+                    result = value2 != 0 ?value1 / value2 : 0.0d ;
+                break;
+
+            default:
+                System.out.println("invalid opCode: " + opCode);
+                result = 0.0d;
+                break;
+            }
+            // you can do it with a shorter way arrow function using switch (opCode) {
+        //            case 'a' -> result = value1 + value2;
+        //            case 's' -> result = value1 - value2;
+        //            case 'm' -> result = value1 * value2;
+        //            case 'd' -> result = value2 != 0 ? value1 / value2 : 0.0d;
+        //            default -> {
+        //                System.out.println("invalid opCode: " + opCode);
+        //                result = 0.0d;
+        //            }
+        //
 
         System.out.println(result);
+
     }
 }
